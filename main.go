306b470f6750740c86133ff1d26ebb12f80c1e23
nicktming <2279563278@qq.com>
2019-06-17 23:20:28 +0800

    add myflannel

---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	log "github.com/golang/glog"
+)
+
+type flagSlice []string
+
+func (t *flagSlice) String() string {
+	return fmt.Sprintf("%v", *t)
+}
+
+func (t *flagSlice) Set(val string) error {
+	*t = append(*t, val)
+	return nil
+}
+
+type CmdLineOpts struct {
+	etcdEndpoints          string
+	etcdPrefix             string
+	etcdKeyfile            string
+	etcdCertfile           string
+	etcdCAFile             string
+	etcdUsername           string
+	etcdPassword           string
+	help                   bool
+	version                bool
+	kubeSubnetMgr          bool
+	kubeApiUrl             string
+	kubeAnnotationPrefix   string
+	kubeConfigFile         string
+	iface                  flagSlice
+	ifaceRegex             flagSlice
+	ipMasq                 bool
+	subnetFile             string
+	subnetDir              string
+	publicIP               string
+	subnetLeaseRenewMargin int
+	healthzIP              string
+	healthzPort            int
+	charonExecutablePath   string
+	charonViciUri          string
+	iptablesResyncSeconds  int
+	iptablesForwardRules   bool
+	netConfPath            string
+}
+
+var (
+	opts           CmdLineOpts
+	errInterrupted = errors.New("interrupted")
+	errCanceled    = errors.New("canceled")
+	flannelFlags   = flag.NewFlagSet("flannel", flag.ExitOnError)
+)
+
+func init() {
+	flannelFlags.StringVar(&opts.etcdEndpoints, "etcd-endpoints", "http://127.0.0.1:4001,http://127.0.0.1:2379", "a comma-delimited list of etcd endpoints")
+	flannelFlags.StringVar(&opts.etcdPrefix, "etcd-prefix", "/coreos.com/network", "etcd prefix")
+	flannelFlags.StringVar(&opts.etcdKeyfile, "etcd-keyfile", "", "SSL key file used to secure etcd communication")
+	flannelFlags.StringVar(&opts.etcdCertfile, "etcd-certfile", "", "SSL certification file used to secure etcd communication")
+	flannelFlags.StringVar(&opts.etcdCAFile, "etcd-cafile", "", "SSL Certificate Authority file used to secure etcd communication")
+	flannelFlags.StringVar(&opts.etcdUsername, "etcd-username", "", "username for BasicAuth to etcd")
+	flannelFlags.StringVar(&opts.etcdPassword, "etcd-password", "", "password for BasicAuth to etcd")
+	flannelFlags.Var(&opts.iface, "iface", "interface to use (IP or name) for inter-host communication. Can be specified multiple times to check each option in order. Returns the first match found.")
+	flannelFlags.Var(&opts.ifaceRegex, "iface-regex", "regex expression to match the first interface to use (IP or name) for inter-host communication. Can be specified multiple times to check each regex in order. Returns the first match found. Regexes are checked after specific interfaces specified by the iface option have already been checked.")
+	flannelFlags.StringVar(&opts.subnetFile, "subnet-file", "/run/flannel/subnet.env", "filename where env variables (subnet, MTU, ... ) will be written to")
+	flannelFlags.StringVar(&opts.publicIP, "public-ip", "", "IP accessible by other nodes for inter-host communication")
+	flannelFlags.IntVar(&opts.subnetLeaseRenewMargin, "subnet-lease-renew-margin", 60, "subnet lease renewal margin, in minutes, ranging from 1 to 1439")
+	flannelFlags.BoolVar(&opts.ipMasq, "ip-masq", false, "setup IP masquerade rule for traffic destined outside of overlay network")
+	flannelFlags.BoolVar(&opts.kubeSubnetMgr, "kube-subnet-mgr", false, "contact the Kubernetes API for subnet assignment instead of etcd.")
+	flannelFlags.StringVar(&opts.kubeApiUrl, "kube-api-url", "", "Kubernetes API server URL. Does not need to be specified if flannel is running in a pod.")
+	flannelFlags.StringVar(&opts.kubeAnnotationPrefix, "kube-annotation-prefix", "flannel.alpha.coreos.com", `Kubernetes annotation prefix. Can contain single slash "/", otherwise it will be appended at the end.`)
+	flannelFlags.StringVar(&opts.kubeConfigFile, "kubeconfig-file", "", "kubeconfig file location. Does not need to be specified if flannel is running in a pod.")
+	flannelFlags.BoolVar(&opts.version, "version", false, "print version and exit")
+	flannelFlags.StringVar(&opts.healthzIP, "healthz-ip", "0.0.0.0", "the IP address for healthz server to listen")
+	flannelFlags.IntVar(&opts.healthzPort, "healthz-port", 0, "the port for healthz server to listen(0 to disable)")
+	flannelFlags.IntVar(&opts.iptablesResyncSeconds, "iptables-resync", 5, "resync period for iptables rules, in seconds")
+	flannelFlags.BoolVar(&opts.iptablesForwardRules, "iptables-forward-rules", true, "add default accept rules to FORWARD chain in iptables")
+	flannelFlags.StringVar(&opts.netConfPath, "net-config-path", "/etc/kube-flannel/net-conf.json", "path to the network configuration file")
+
+	// glog will log to tmp files by default. override so all entries
+	// can flow into journald (if running under systemd)
+	flag.Set("logtostderr", "true")
+
+	// Only copy the non file logging options from glog
+	copyFlag("v")
+	copyFlag("vmodule")
+	copyFlag("log_backtrace_at")
+
+	// Define the usage function
+	flannelFlags.Usage = usage
+
+	// now parse command line args
+	flannelFlags.Parse(os.Args[1:])
+}
+
+func copyFlag(name string) {
+	flannelFlags.Var(flag.Lookup(name).Value, flag.Lookup(name).Name, flag.Lookup(name).Usage)
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s [OPTION]...\n", os.Args[0])
+	flannelFlags.PrintDefaults()
+	os.Exit(0)
+}
+
+func main()  {
+	log.Info("starting")
+}
